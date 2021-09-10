9b5299602277780f1715f655e016b014a2a8a793
Hopperys <vano_ust.kz@mail.ru>
2021-09-10 21:07:59 +0600

    Правки

--- build/js/main.js
+++ build/js/main.js
@@ -1,77 +1,89 @@
 'use strict';
 
-if (document.contains(document.querySelector('nav'))) {
-  var navbarList = document.querySelector('.navbar ul');
+(function () {
+  if (document.contains(document.querySelector('nav'))) {
+    var navbarList = document.querySelector('.navbar ul');
 
-  navbarList.addEventListener('click', function (evt) {
-    evt.preventDefault();
-    var blockId = evt.target.getAttribute('href');
+    navbarList.addEventListener('click', function (evt) {
+      evt.preventDefault();
+      var blockId = evt.target.getAttribute('href');
 
-    document.querySelector(blockId).scrollIntoView({
-      behavior: 'smooth',
-      block: 'start'
+      if (document.contains(document.querySelector(blockId))) {
+        document.querySelector(blockId).scrollIntoView({
+          behavior: 'smooth',
+          block: 'start'
+        });
+      }
     });
-  });
 
-  var navbar = document.querySelector('.navbar');
-  var header = document.querySelector('.page-header');
-  var burger = navbar.querySelector('.navbar__burger');
+    var navbar = document.querySelector('.navbar');
+    var header = document.querySelector('.page-header');
+    var burger = navbar.querySelector('.navbar__burger');
 
-  navbar.classList.remove('navbar--nojs');
-  header.classList.remove('page-header--nojs');
+    navbar.classList.remove('navbar--nojs');
+    header.classList.remove('page-header--nojs');
 
-  burger.addEventListener('click', function () {
-    if (navbar.classList.contains('navbar--closed')) {
-      navbar.classList.remove('navbar--closed');
-      navbar.classList.add('navbar--opened');
-    } else {
-      navbar.classList.add('navbar--closed');
-      navbar.classList.remove('navbar--opened');
-    }
-  });
-}
-
-if (document.contains(document.querySelector('form'))) {
-  var information = document.querySelector('.information');
-  var form = information.querySelector('form');
-  var phoneInput = form.querySelector('#phone');
-  var nameInput = form.querySelector('#name');
-
-  var validity = /^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$/;
-
-  var isStorageSupport = true;
-  var storage = {};
-
-  try {
-    storage.name = localStorage.getItem('name');
-    storage.phone = localStorage.getItem('phone');
-  } catch (err) {
-    isStorageSupport = false;
+    burger.addEventListener('click', function () {
+      if (navbar.classList.contains('navbar--closed')) {
+        navbar.classList.remove('navbar--closed');
+        navbar.classList.add('navbar--opened');
+      } else {
+        navbar.classList.add('navbar--closed');
+        navbar.classList.remove('navbar--opened');
+      }
+    });
   }
 
-  window.addEventListener('load', function () {
-    if (storage) {
-      phoneInput.value = storage.phone;
-      nameInput.value = storage.name;
+})();
+
+(function () {
+  if (document.contains(document.querySelector('form'))) {
+    var information = document.querySelector('.information');
+    var form = information.querySelector('form');
+    var phoneInput = form.querySelector('#phone');
+    var nameInput = form.querySelector('#name');
+
+    var isStorageSupport = true;
+    var storage = {};
+
+    try {
+      storage.name = localStorage.getItem('name');
+      storage.phone = localStorage.getItem('phone');
+    } catch (err) {
+      isStorageSupport = false;
     }
-  });
 
-  form.addEventListener('submit', function (evt) {
-    evt.preventDefault();
+    window.addEventListener('load', function () {
+      if (storage) {
+        phoneInput.value = storage.phone;
+        nameInput.value = storage.name;
+      }
+    });
 
-    if (!validity.test(phoneInput.value)) {
-      phoneInput.classList.add('information__error');
-    }
+    form.addEventListener('submit', function (evt) {
+      evt.preventDefault();
 
-    if (isStorageSupport) {
-      localStorage.setItem('name', nameInput.value);
-      localStorage.setItem('phone', phoneInput.value);
-    }
+      if (isStorageSupport) {
+        localStorage.setItem('name', nameInput.value);
+        localStorage.setItem('phone', phoneInput.value);
+      }
 
-    if (validity.test(phoneInput.value) && nameInput.value !== '') {
-      phoneInput.classList.remove('information__error');
-      form.submit();
-    }
-  });
+      if (phoneInput.value === '') {
+        phoneInput.classList.add('information__error');
+      }
 
-}
+      if (nameInput.value === '') {
+        nameInput.classList.add('information__error');
+      }
+
+      if (phoneInput.value !== '' && nameInput.value !== '') {
+        nameInput.classList.remove('information__error');
+        phoneInput.classList.remove('information__error');
+        form.submit();
+      }
+    });
+  }
+
+})();
+
+
